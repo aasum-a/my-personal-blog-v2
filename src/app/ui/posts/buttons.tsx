d1488a639b6988c96d46db98c683fe70be2396f0
aasum-a <andymuhammadsandika@gmail.com>
2025-07-19 21:32:50 +0700

    feat(buttons): fix issues in DeletePostButton

--- src/app/ui/posts/buttons.tsx
+++ src/app/ui/posts/buttons.tsx
@@ -2,10 +2,6 @@
 
 import Link from "next/link";
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { deletePost, State } from "@/lib/actions";
-import { useActionState, useEffect } from "react";
-import { useFormStatus } from "react-dom";
-import toast from "react-hot-toast";
 
 export function UpdatePostButton({ id }: { id: string }) {
   return (
@@ -18,35 +14,13 @@ export function UpdatePostButton({ id }: { id: string }) {
   );
 }
 
-function DeleteButton() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="p-2 rounded-md border hover:bg-gray-100 disabled:bg-gray-200"
-      disabled={pending}
-    >
-      <span className="sr-only">Hapus</span>
-      <TrashIcon className="w-5 text-red-500" />
-    </button>
-  );
-}
-
 export function DeletePostButton({ id }: { id: string }) {
-  const initialState: State = { message: null, errors: {} };
-  const deletePostWithId = deletePost.bind(null, id);
-  const [state, formAction] = useActionState(deletePostWithId, initialState);
-
-  useEffect(() => {
-    if (state?.message?.includes("berhasil")) {
-      toast.success(state.message);
-    } else if (state?.message) {
-      toast.error(state.message);
-    }
-  }, [state]);
-
   return (
-    <form action={formAction}>
-      <DeleteButton />
-    </form>
+    <>
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Hapus</span>
+        <TrashIcon className="w-5" />
+      </button>
+    </>
   );
 }
